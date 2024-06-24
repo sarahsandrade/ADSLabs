964e7970770e751a1ba2e9e55680c3eb07970aa2
Sarah <sarah2017.andrade@gmail.com>
2024-06-23 23:18:45 -0300

    Arrumei a problema de verificar data

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -1,7 +1,7 @@
 function validacaoR (req, res, next){
 
     if(!req.body.data_nascimento)
-        return res.status(400).send({messege: "favor enviar data de nascimento"})
+         return res.status(400).send({messege: "favor enviar data de nascimento"})
     if(nascimento(req.body.data_nascimento))
         return res.status(400).send({messege: "responsavel tem que ter nascido no minimo em 2014"})
     if(!req.body.nome)
@@ -19,7 +19,7 @@ function letras(str){
 }
 function nascimento (date){
     let data1= new Date (date)
-    let data2= new Date ('2014')
+    let data2= new Date ('2015')
     return data1.getFullYear() > data2.getFullYear()
 }
 module.exports = {validacaoR}
